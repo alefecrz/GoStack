c36203801233bee11a0ad66208c8a47ba0018696
Alefe Cruz <alefecrz@gmail.com>
2021-06-04 23:46:18 -0300

    feat: create project in route post

--- Nivel-1/Aulas/Backend-Node/src/index.js
+++ Nivel-1/Aulas/Backend-Node/src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
-const app = express();
 const PORT = 3333;
+const app = express();
 
+app.use(express.json());
 
-const projects = [];
+const projectList = [];
 app.get('/projects', (request, response) => { 
-  return response.status(200).json(projects);
+  return response.status(200).json(projectList);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  
+  const project = { id: uuid(), title, owner };
+
+  projectList.push(project);
+  return response.status(201).json(project);
 });
 
 app.listen(PORT, () => console.log("Running in port: ", PORT) );
