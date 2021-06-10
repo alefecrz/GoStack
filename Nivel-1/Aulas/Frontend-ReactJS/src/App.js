dface37bc9d7b69e2c24bbd6f4ab49f69a08032a
Alefe Cruz <alefecrz@gmail.com>
2021-06-09 23:08:47 -0300

    feat: create component header

--- Nivel-1/Aulas/Frontend-ReactJS/src/App.js
+++ Nivel-1/Aulas/Frontend-ReactJS/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello GoStack</h1>;
+import Header from './components/Header';
+
+const App = () => {
+  return (<>
+    <Header />
+    <Header />
+    <Header />
+  </>);
 }
 
 export default App;
