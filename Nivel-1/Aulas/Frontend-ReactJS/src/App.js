3ff82d38c6908de8fb93a799b06f9cb1ce3145e4
Alefe Cruz <alefecrz@gmail.com>
2021-06-11 23:56:39 -0300

    feat: create project via frontEnd

--- Nivel-1/Aulas/Frontend-ReactJS/src/App.js
+++ Nivel-1/Aulas/Frontend-ReactJS/src/App.js
@@ -8,34 +8,36 @@ import './App.css'
 
 const App = () => {
   const [projects, setProjects] = useState([]);
-  const [project, setProject] = useState('');
+  const [title, setTitle] = useState('');
+  const [owner, setOwner] = useState('');
 
   useEffect(() => {
     api.get('/projects').then(response => {
-      console.log(response);
+      const { data } = response;
+      setProjects(data);
     });
   }, []);
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    setProject(event.target.value);
-  }
-
   const handleAddproject = () => {
-    if (!!project){
-      const date = new Date();
-      const newProject = `${project + date.getTime()}`
-      setProjects([... projects, newProject ]);
+    if (!!title && !!owner){
+      api.post('/projects', { title, owner }).then( response => {
+        const { data } = response;
+        setProjects([...projects , data]);
+      })
     }
   };
 
   return (
     <>
       <Header title="ADD PROJECTS" />
-      <input type="text" value={project} onChange={event => handleChange(event)}/>
+      <form>
+        <input type="text" value={title} onChange={event => setTitle(event.target.value)}/>
+        <br/>
+        <input type="text" value={owner} onChange={event => setOwner(event.target.value)}/>
+      </form>
       <button onClick={() => handleAddproject()}>adicionar projeto</button>
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title} {project.owner}</li>)}
       </ul>
     </>
   );
